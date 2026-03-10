9d2a96eab13fbafccc115da9c12b764f2e8fb0d4
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-03-10 13:03:28 +0530

    Initilize the backend 😎

---
+++ server/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get("/", (req, res) => {
+  res.send("Lost & Found API Running");
+});
+
+
+
+const PORT = process.env.PORT 
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log("MongoDB Connected ✅");
+  app.listen(PORT, () => console.log(`Server running on ${PORT} ✅`));
+})
+.catch(err => console.log(err));

707280271f9c6ec69a691ec3100bd45ad830d766
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-03-11 20:55:04 +0530

    feat: add initial page components for Browse, Home, ItemDetails, ReportFound, and ReportLost 😒

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Lost & Found Platform</h1>
+      <p>Browse lost and found items</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ client/src/pages/ItemDetails.jsx
@@ -0,0 +1,15 @@
+import { useParams } from "react-router-dom";
+
+function ItemDetails() {
+
+  const { id } = useParams();
+
+  return (
+    <div>
+      <h2>Item Details</h2>
+      <p>Item ID: {id}</p>
+    </div>
+  );
+}
+
+export default ItemDetails;

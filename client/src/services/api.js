8d184c8672e110379d493b80778ede863979a674
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-03-13 10:55:41 +0530

    feat: add API functions for creating and retrieving items 😪

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,4 +4,8 @@ const API = axios.create({
   baseURL: "http://localhost:3005/api"
 });
 
+export const createItem = (data) => API.post("/items", data);
+
+export const getItems = () => API.get("/items");
+
 export default API;
